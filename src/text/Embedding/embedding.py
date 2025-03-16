- Fixed no gradients, when using the last vector as the aggregated embedding. - For some reason cpu and gpu utilization are below 10% at all times during training now. This needs to be addressed next.

src/text/Embedding/embedding.py
@@ -62,24 +62,28 @@ class Embedding(ABC):
         embeddings = []
 
         #for sentence in sentences:
-        for i in range(len(sentences)):
-            print("|", end="")
-            sentence = sentences[i]
-            mask = masks[i] if masks is not None else None
-            words = self.get_words(sentence, seperator)
-            if len(words) > padding_length:
-                words = words[:padding_length]
-            elif len(words) < padding_length and masks is not None:
-                mask = mask[:len(words)]
-            embedded = self.embed_words(words, mask, aggregate)
-            if not aggregate:
-                if embedded.shape[0] < padding_length:
-                    embedded = torch.nn.functional.pad(embedded, (0, 0, 0, padding_length - embedded.shape[0]))
-                #elif embedded.shape[0] > padding_length > 0:
-                    #embedded = embedded[:padding_length]
-                if embedded.shape[0] != padding_length:
-                    raise ValueError(f"Expected shape of {padding_length}, but got {embedded.shape[0]}")
-            embeddings.append(embedded)
+        print("Embedding sentences...")
+        ui = ConsoleUserInterface()
+        with ui.display():
+            for i in range(len(sentences)):
+                #print("|", end="")
+                ui.update(f"Embedding sentence {i + 1}/{len(sentences)}")
+                sentence = sentences[i]
+                mask = masks[i] if masks is not None else None
+                words = self.get_words(sentence, seperator)
+                if len(words) > padding_length:
+                    words = words[:padding_length]
+                elif len(words) < padding_length and masks is not None:
+                    mask = mask[:len(words)]
+                embedded = self.embed_words(words, mask, aggregate)
+                if not aggregate:
+                    if embedded.shape[0] < padding_length:
+                        embedded = torch.nn.functional.pad(embedded, (0, 0, 0, padding_length - embedded.shape[0]))
+                    #elif embedded.shape[0] > padding_length > 0:
+                        #embedded = embedded[:padding_length]
+                    if embedded.shape[0] != padding_length:
+                        raise ValueError(f"Expected shape of {padding_length}, but got {embedded.shape[0]}")
+                embeddings.append(embedded)
         try:
             stacked =  torch.stack(embeddings)
         except RuntimeError as e:

src/text/Embedding/huggingmodel.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 from numpy import ndarray
 from torch import Tensor
+import torch.nn.functional as F
 
 from .embedding import Embedding
 from .tokenizer import Tokenizer
@@ -114,9 +115,7 @@ class HuggingModel(Tokenizer, Embedding, ABC):
             word_embeddings = torch.concat((word_embeddings, word_embedding), dim=0)
         return word_embeddings
 
-    def embed_words(self, words: List[str], mask: Optional[Tensor] = None, aggregate:bool = False) -> Tensor:
-        if aggregate:
-            raise NotImplementedError
+    def _embed_words_full(self, words: List[str], mask: Optional[Tensor] = None) -> Tensor:
         tokenized = [self.tokenize(word) for word in words]
         if mask is None:
             mask = torch.ones(len(words)).to(self.device)
@@ -129,7 +128,20 @@ class HuggingModel(Tokenizer, Embedding, ABC):
         masked = aggregated * mask.unsqueeze(1).expand_as(aggregated)
         return masked
 
-    @abstractmethod
+    def _embed_words_last(self, words: List[str], mask: Optional[Tensor] = None) -> Tensor:
+        classification_added_words = words + ["I", "am", "feeling"]
+        added_mask = Tensor([1, 1, 1]).to(self.device) if mask is not None else None
+        classification_added_mask = torch.concat((mask, added_mask)) if mask is not None else None
+        masked = self._embed_words_full(classification_added_words, classification_added_mask)
+        last_entry = masked[-1]
+        expanded = last_entry.unsqueeze(0)
+        return expanded
+
+    def embed_words(self, words: List[str], mask: Optional[Tensor] = None, aggregate: bool = True) -> Tensor:
+        if aggregate:
+            return self._embed_words_last(words, mask)
+        return self._embed_words_full(words, mask)
+
     def embed_tokenized(self, tokenized: Tensor) -> Tensor:
         """
         This method takes a list of token indices and returns the
@@ -138,7 +150,13 @@ class HuggingModel(Tokenizer, Embedding, ABC):
         :param tokenized: A tensor of token indices of shape (num_tokens).
         :return: A two-dimensional Tensor where each token index is an embedding. (num_tokens, embedding_size)
         """
-        pass
+        max_length = self._tokenizer.model_max_length
+        token_vec = tokenized[:max_length]
+        input_embeds_mat = self.model.get_input_embeddings().weight.data
+        one_hot = (F.one_hot(token_vec.long(), input_embeds_mat.shape[0]).float() + (
+                    token_vec - token_vec.detach()).unsqueeze(1)).to(input_embeds_mat.dtype)
+        inputs_embeds = one_hot @ input_embeds_mat
+        return inputs_embeds
 
     @abstractmethod
     def fully_embed_tokenized(self, tokenized: Tensor, mask: Optional[Tensor] = None) -> Tensor:

src/text/Embedding/llama.py
@@ -29,21 +29,15 @@ class LLama(HuggingModel):
         model = AutoModel.from_pretrained(self._model_prefix + self.get_model_name(), trust_remote_code=True, torch_dtype=torch.float16)
         return model
 
-    def embed_tokenized(self, tokenized: Tensor) -> Tensor:
-        max_length = self._tokenizer.model_max_length
-        token_vec = tokenized[:max_length]
-        input_embeds_mat = self.model.get_input_embeddings().weight.data
-        one_hot = (F.one_hot(token_vec.long(), input_embeds_mat.shape[0]).float() + (
-                    token_vec - token_vec.detach()).unsqueeze(1)).to(input_embeds_mat.dtype)
-        inputs_embeds = one_hot @ input_embeds_mat
-        return inputs_embeds
-
     def fully_embed_tokenized(self, tokenized: Tensor, mask: Optional[Tensor] = None) -> Tensor:
         input_embeds = self.embed_tokenized(tokenized).unsqueeze(0)
         mask = mask.unsqueeze(0) if mask is not None else None
         if mask is not None:
-            #outputs = self.model(inputs_embeds=input_embeds, attention_mask=mask)
             causal_mask = self._get_4d_causal_mask(mask)
+            input_embeds = input_embeds.to(self.model.get_input_embeddings().weight.data.dtype)
+            #normal_outputs = self.model(inputs_embeds=input_embeds, attention_mask=mask, output_attentions=True)
+            torch.backends.cuda.enable_mem_efficient_sdp(False)
+            torch.backends.cuda.enable_flash_sdp(False)
             outputs = self.model(inputs_embeds=input_embeds, attention_mask=causal_mask)
             #if not torch.allclose(outputs[0], causal_output[0]):
                 #expected = [o for o_list_list in (outputs[0].tolist()) for o_list in o_list_list for o in o_list]
@@ -98,17 +92,6 @@ class LLama(HuggingModel):
 
     def decode2tokenized(self, embedding: List[np.ndarray]) -> List[int]:
         raise NotImplementedError
-
-    def embed_words(self, words: List[str], mask: Optional[Tensor] = None, aggregate: bool = True) -> Tensor:
-        if not aggregate:
-            return super().embed_words(words, mask)
-        classification_added_words = words + ["I", "am", "feeling"]
-        added_mask = Tensor([1, 1, 1]).to(self.device) if mask is not None else None
-        classification_added_mask = torch.concat((mask, added_mask)) if mask is not None else None
-        masked = super().embed_words(classification_added_words, classification_added_mask)
-        last_entry = masked[-1]
-        expanded = last_entry.unsqueeze(0)
-        return expanded
 
     def get_model_name(self)->str:
         return "Llama-3.2-1B"