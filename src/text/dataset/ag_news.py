750e450dfdb5f6365cdf2b2a87fcd906aceb3bf3
Alex Jenke <alex.jenke@outlook.com>
2025-04-14 15:39:02 +0200

    Made AG News Transformer aggregatable.

--- src/text/dataset/ag_news.py
+++ src/text/dataset/ag_news.py
@@ -5,17 +5,29 @@ from typing import Tuple, List
 import numpy as np
 import pandas as pd
 
-from .dataset import Dataset
+from .dataset import Dataset, AggregatableDataset
 
 X = "Description"
 Y = "Class Index"
 
 
-class AGNews(Dataset):
+class AGNews(AggregatableDataset):
     """
     AG News Dataset class. It reads the train and test files from the resource folder and stores them as pandas dataframes.
     Consists of class ids 1-4 where 1-World, 2-Sports, 3-Business, 4-Sci/Tech
     """
+
+    def prefix(self) -> List[str]:
+        return (#"Description: TOKYO, April 14 (Reuters) - Japanese Prime Minister Shigeru Ishiba said on Monday his country does not plan to make big concessions and won't rush to reach a deal in upcoming tariff negotiations with U.S. President Donald Trump's administration."
+                #"News type: World"
+                "Description : EU foreign ministers meet in wake of deadly Russian attack on Sumy as Zelenskyy issues plea for Trump to visit Ukraine – Europe live."
+                "News type : World\n"
+                "Description : Stocks rally as electronics get a tariff break."
+                "News type : Business\n"
+                "Description :").split(" ")
+
+    def suffix(self) -> List[str]:
+        return ". News type :".split(" ")
 
     def get_possible_labels(self) -> list:
         return [1, 2, 3, 4]
