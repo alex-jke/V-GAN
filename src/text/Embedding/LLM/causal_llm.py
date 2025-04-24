Fixed bug in embedding logic - Causal LLM was outputting token logits instead of the embeddings. - Removed standardization and normalization from embedding_space.py. - switched order of meaning and norming in huggingmodel.py. Now, its first meaned and then normalized. This, way the vectors are guaranteed to be of length 1.

src/text/Embedding/LLM/causal_llm.py
@@ -89,9 +89,12 @@ class CausalLLM(HuggingModel, ABC):
                                       f"larger than {self.max_token_length()} will be trimmed. No further warnings will be given.")
                         self._token_length_warning_given = True
                     tokenized = tokenized[:self.max_token_length()]
-                outputs = self.model(input_ids=tokenized.int().unsqueeze(0), use_cache=False)
+                unsqueezed = tokenized.int().unsqueeze(0)
+                outputs = self.model(input_ids=unsqueezed, use_cache=False, output_hidden_states=True)
             outputs = outputs # Otherwise the line below complains about usage before assignment.
-        embeddings = outputs[0]
-        de_batched = embeddings[0]
-        return de_batched
+        embeddings = outputs.hidden_states
+        last_layer = embeddings[-1] #TODO: check if this is correct.
+        de_batched = last_layer[0]
+        normalized = torch.nn.functional.normalize(de_batched)
+        return normalized
 

src/text/Embedding/LLM/huggingmodel.py
@@ -203,9 +203,9 @@ class HuggingModel(Tokenizer, Embedding, ABC):
         aggregated = self._aggregate_token_to_word_embedding(embeddings, tokenized)
 
         # Mean the embeddings. Otherwise, setting an embedding to zero might change the meaning.
-        normed = torch.nn.functional.normalize(aggregated, dim=1)
-        meaned = normed.mean(dim=0)
-        aggregated = normed - meaned
+        meaned = aggregated - aggregated.mean(dim=0)
+        normed = torch.nn.functional.normalize(meaned, dim=1)
+        aggregated = normed
 
         # Apply the mask to the embeddings, so that the masked tokens are zeroed out
         masked = aggregated * mask.unsqueeze(1).expand_as(aggregated) if mask is not None else aggregated
@@ -324,9 +324,9 @@ class HuggingModel(Tokenizer, Embedding, ABC):
                 #aggregated = self.aggregateEmbeddings(embeddings = embeddings)
             if batch_first:
                 transposed = embeddings.T
-                normed = torch.nn.functional.normalize(transposed, dim=1)
-                meaned = normed - normed.mean(dim=0)
-                return meaned
+                meaned = transposed - transposed.mean(dim=0)
+                normed = torch.nn.functional.normalize(meaned, dim=1)
+                return normed
             return embeddings
         return embedding
 

src/text/outlier_detection/space/embedding_space.py
@@ -46,9 +46,11 @@ class EmbeddingSpace(Space):
     def __prepare_embedding(self, tokenized: Tensor) -> Tensor:
         embedding_func = self.model.get_embedding_fun(batch_first=True)
         embedded = embedding_func(tokenized)
-        means = embedded.mean(1, keepdim=True)
-        stds = embedded.std(1, keepdim=True)
-        standardized = (embedded - means) / stds
-        normalized = torch.nn.functional.normalize(standardized, p=2, dim=1)
-        return normalized
+        #means = embedded.mean(1, keepdim=True)
+        #stds = embedded.std(1, keepdim=True)
+        #standardized = (embedded - means) / stds
+        #normalized = torch.nn.functional.normalize(standardized, p=2, dim=1)
+        #normalized = torch.nn.functional.normalize(embedded, p=2, dim=1)
+        #return normalized
+        return embedded
 