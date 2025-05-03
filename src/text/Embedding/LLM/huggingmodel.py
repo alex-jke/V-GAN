small changes to logic - All subspaces are now used instead of only those larger than 0.2%, when rest is above 80% - removed normalization of embeddings from embed words - fixed bug in Text V Odm causing it to compute the full space even when not necessary. - increased number of subspaces used for V ODM from 50 to 200

src/text/Embedding/LLM/huggingmodel.py
@@ -202,7 +202,7 @@ class HuggingModel(Tokenizer, Embedding, ABC):
         embeddings = self.fully_embed_tokenized(tokenized_tensor, expanded_mask)
         aggregated = self._aggregate_token_to_word_embedding(embeddings, tokenized)
 
-        aggregated = torch.nn.functional.normalize(aggregated, dim=1)
+        #aggregated = torch.nn.functional.normalize(aggregated, dim=1)
 
         # Apply the mask to the embeddings, so that the masked tokens are zeroed out
         masked = aggregated * mask.unsqueeze(1).expand_as(aggregated) if mask is not None else aggregated
@@ -232,9 +232,9 @@ class HuggingModel(Tokenizer, Embedding, ABC):
 
     def embed_words(self, words: List[str], mask: Optional[Tensor] = None, strategy: StrategyInstance = UnificationStrategy.TRANSFORMER.create()) -> Tensor:
         if strategy.equals(UnificationStrategy.TRANSFORMER):
-            return self._embed_words_last(words, mask)
+            return torch.nn.functional.normalize(self._embed_words_last(words, mask), dim=1)
         if strategy.equals(UnificationStrategy.MEAN):
-            return self._embed_words_full(words, mask).mean(dim=0).unsqueeze(0)
+            return torch.nn.functional.normalize(self._embed_words_full(words, mask).mean(dim=0).unsqueeze(0), dim=1)
         if strategy.equals(UnificationStrategy.PADDING):
             return self._embed_words_full(words, mask)
         raise NotImplementedError(f"The strategy {strategy} is not implemented for huggingmodels.")
@@ -325,9 +325,11 @@ class HuggingModel(Tokenizer, Embedding, ABC):
                 #aggregated = self.aggregateEmbeddings(embeddings = embeddings)
             if batch_first:
                 transposed = embeddings.T
-                meaned = transposed - transposed.mean(dim=0)
-                normed = torch.nn.functional.normalize(meaned, dim=1)
+                #meaned = transposed - transposed.mean(dim=0)
+                normed = torch.nn.functional.normalize(transposed, dim=1)
+
                 return normed
+                #return transposed
             return embeddings
         return embedding
 

src/text/outlier_detection/space/token_space.py
@@ -53,9 +53,10 @@ class TokenSpace(Space):
                 # Remove padding tokens
                 sample = sample[sample != self.model.padding_token]
 
-                embedded_tokens = self.model.fully_embed_tokenized(sample)
-                #embedding = embedded_tokens.mean(dim=0)
-                embedding = self.model.aggregateEmbeddings(embedded_tokens)
+                embedded_tokens = self.model.fully_embed_tokenized(sample) # TODO: normalize?
+                embedding = embedded_tokens.mean(dim=0)
+                #embedding = self.model.aggregateEmbeddings(embedded_tokens)
+                assert len(embedding.shape) == 1, "Embedding should be 1D."
                 embeddings.append(embedding)
         return torch.stack(embeddings)
 

src/text/outlier_detection/word_based_v_method/text_v_odm.py
@@ -94,16 +94,16 @@ class TextVOdm(EnsembleODM):
         return ensemble_scores
 
     def _predict(self):
-        subspace_dists = torch.zeros_like(self.y_test).cpu()
+        subspace_dists = torch.zeros_like(self.embedded_data[0].y_test).cpu()
         if self.subspace_distance_lambda != 0:
             subspace_dists = self._calculate_distances(self.x_test, self.embedded_data, self.space).cpu()
 
-        ensemble_scores = torch.zeros_like(self.y_test).cpu()
+        ensemble_scores = torch.zeros_like(self.embedded_data[0].y_test).cpu()
         if self.classifier_delta != 0:
             ensemble_scores = self._predict_ensemble().cpu()
         # Combine the two scores
         combined_scores = self.classifier_delta * ensemble_scores + self.subspace_distance_lambda * subspace_dists
-        assert len(combined_scores.shape) == 1 and combined_scores.shape == self.y_test.shape
+        assert len(combined_scores.shape) == 1 and combined_scores.shape == self.embedded_data[0].y_test.shape
         self.predictions = combined_scores
 
     def _get_name(self) -> str:

src/vmmd.py
@@ -135,8 +135,7 @@ class VMMD(VMMDBase):
                     yield epoch
 
         self.generator = generator
-        if self.export_generator:
-            self._export(generator)
+        self._export(generator, export_params=self.export_generator)
 
 def model_eval(model, X_data) -> pd.DataFrame:
     device = torch.device('cuda' if torch.cuda.is_available(